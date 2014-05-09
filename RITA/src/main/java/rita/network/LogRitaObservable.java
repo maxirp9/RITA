f19c713ac9aa1b93674d5bcae618f738474ba0ad
pvilaltella <vilaltellapablo@gmail.com>
2014-05-09 15:14:19 -0300

    Agregando el texto en el log

---
+++ RITA/src/main/java/rita/network/LogRitaObservable.java
@@ -0,0 +1,27 @@
+package rita.network;
+
+import java.util.Observable;
+
+/**
+ * @author pvilaltella
+ * 
+ * Clase para guardar todo lo relevante que debe mostrar el log del servidor
+ *
+ */
+public class LogRitaObservable extends Observable{
+
+	public LogRitaObservable() {
+		super();
+	}
+
+	/**
+	 * Setea el arreglo de log y Notifica a los observadores del cambio
+	 * @param textoLog
+	 */
+	public void changeData(String textoLog) {
+		
+		setChanged();
+		notifyObservers(textoLog);
+	}
+
+}
