6ffad07896adb2131c4249cf1ff33c25e99069da
maximiliano <maxirp9@gmail.com>
2014-11-25 19:18:09 -0300

    Icono de espera y correciones de interfaz

---
+++ RITA/src/main/java/rita/network/Cursors.java
@@ -0,0 +1,10 @@
+package rita.network;
+
+import java.awt.Cursor;
+
+public interface Cursors {
+	  Cursor WAIT_CURSOR = 
+	    Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR);
+	  Cursor DEFAULT_CURSOR = 
+	    Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR);  
+	}
